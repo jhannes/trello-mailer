3dc0ea5db4acc977696853fef7a2041562aeb1e3
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2018-02-02 20:12:26 +0100

    find-sponsors.js extracts matching emails from IMAP

--- find-sponsors.js
+++ find-sponsors.js
@@ -1,4 +1,5 @@
 const debugImap = require('debug')('mail:imap');
+const addrs = require("email-addresses");
 
 var imaps = require('imap-simple');
 const settings = require('./settings.local.js'); 
@@ -13,42 +14,40 @@ var config = {
   }
 };
 
-function searchEmails() {
-    imaps.connect(config).then(connection => {
-        return connection.openBox('Sent').then(() => {
-            return connection.search(['ALL'], {bodies: ['HEADER']}).then(results => {
-                const subjects = results.map(res => {
-                    const header = res.parts.filter(p => p.which === 'HEADER')[0].body;
-                    return {
-                        subject: header.subject[0],
-                        to: header.to ? header.to[0] : "to who?"
-                    };
-                });
-                console.log(subjects);
+function summarizeMailbox(connection, box) {
+    return connection.openBox(box)
+    .then(() => connection.search(['ALL'], {bodies: ['HEADER']}))
+    .then(results =>
+        results.map(res => {
+            const header = res.parts.filter(p => p.which === 'HEADER')[0].body;
+            return {
+                subject: header.subject[0],
+                to: header.to ? header.to[0] : "to who?",
+                date: header.date[0],
+                from: header.from[0],
+                header
+            };
+        })
+    );
+}
+
+function mailSummary(boxes) {
+    return imaps.connect(config)
+    .then(connection => {
+        return Promise.all(boxes.map(box => summarizeMailbox(connection, box)))
+            .then(lists => [].concat.apply([], lists))
+            .then(e => {
                 connection.end();
-            });
-        }).catch(err => {
-            connection.end();
-            // eslint-disable-next-line no-console
-            console.error(err);
-        });
+                return e;
+            })
     }).catch(err => {
-        // TODO: This wouldn't be needed if imap-simple used modern Promises
         // eslint-disable-next-line no-console
         console.error(err);
     });    
 }
 
-/*
-const idLists = ['58d4fad19f8050e3ef837a11', '565f53acd8080adcd0c1bb37'];
-
-const {listCardsFromList} = require('./lib/trello.customFields.js');
-Promise.all(idLists.map(listCardsFromList))
-    .then(console.log);
-*/
 
 const CUSTOM_DATA_PLUGIN_ID = '56d5e249a98895a9797bebb9';
-
 
 const {trelloGet} = require('./lib/trello');
 
@@ -56,7 +55,7 @@ function cardsForBoard(boardId) {
     return trelloGet(`1/boards/${boardId}/cards/`, {pluginData: true});
 }
 
-function parsePluginData(card, board) {
+function parseCustomFields(card, board) {
     const boardCustomFields = board.pluginData.find(data => data.idPlugin === CUSTOM_DATA_PLUGIN_ID);
     const cardCustomFields = card.pluginData.find(data => data.idPlugin === CUSTOM_DATA_PLUGIN_ID);
 
@@ -75,52 +74,47 @@ function parsePluginData(card, board) {
     return cardCustomFields ? customFields() : {};
 }
 
-function findCardsInBoard(idBoard, board) {
+function findSponsorsInBoard(idBoard, board) {
     return trelloGet(`1/boards/${idBoard}/lists/`)
         .then(lists => lists.map(list => {return {id: list.id, name: list.name};}))
         .then(lists => {
             return trelloGet(`1/boards/${idBoard}/cards/`, {pluginData: true})
-                .then(cards => cards.map(card => {
-                    return {
-                        id: card.id, name: card.name,
-                        list: lists.filter(l => l.id === card.idList)[0].name,
-                        board: board.name,
-                        pluginData: parsePluginData(card, board)
-                    }
+                .then(cards =>
+                    mailSummary(['Sent', 'Inbox/Archive']).then(mailSummaries => {
+                        return cards.map(card => {
+                            const customData = parseCustomFields(card, board);
+                            let domain = null;
+                            let emails = [];
+                            const mainContact = customData["Main contact (Email)"];
+                            if (mainContact) {
+                                domain = addrs.parseAddressList(mainContact)[0].domain;
+                                emails = mailSummaries.filter(summary =>
+                                    summary.to.indexOf("@" + domain) != -1 || summary.from.indexOf("@" + domain) != -1
+                                );
+                            }
+        
+                            return {
+                                id: card.id, name: card.name,
+                                list: lists.filter(l => l.id === card.idList)[0].name,
+                                board: board.name,
+                                customData,
+                                domain,
+                                emails
+                            }  
+                    })
                 }));
         });
 }
 
-function findAllCards(idBoard) {
-    return trelloGet(`1/boards/${idBoard}`, {pluginData: true}).then(board => findCardsInBoard(idBoard, board))
+
+function findSponsors(idBoard) {
+    return trelloGet(`1/boards/${idBoard}`, {pluginData: true}).then(board => findSponsorsInBoard(idBoard, board));
 }
 
-
-
-
 const idBoards = ['58d4fad19f8050e3ef837a11', '565f53acd8080adcd0c1bb37'];
-//const idBoards = ['565f53acd8080adcd0c1bb37'];
 
-Promise.all(idBoards.map(id => findAllCards(id)))
-    .then(list => {
-        list.forEach(cards => {
-            cards.forEach(card => console.log(`"${card.name.toLowerCase()}": [ "${(card.pluginData["Main contact (Email)"] || "")}" ]`))
-        });
-    })
-
-/*
-Promise.all(idBoards.map(cardsForBoard))
-    .then(all => {
-        all.forEach(result =>
-            //{ result.map(res => console.log({name:res.name, list: res.idList, board: res.idBoard})); }
-            { result.map(res => console.log(res)); }
-        );
-    });
-
-trelloGet('1/organizations/mobileera2016/boards/')
-    .then(boards => boards.map(board => { return {id: board.id, name: board.name};}))
-    .then(console.log)
-.catch(console.error);
-*/
-
-
+Promise.all(idBoards.map(id => findSponsors(id)))
+    .then(lists => [].concat.apply([], lists))
+    .then(sponsors => console.log(JSON.stringify(sponsors)))
+    .catch(err => console.error(err))
+    
